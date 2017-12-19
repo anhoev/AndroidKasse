3d2ee6a97597f5acf657e8fc79417440d8bc1ebc
Anh <anhoev@yahoo.de>
2017-12-19 15:36:15 +0100

    update feature

---
+++ cp.sh
@@ -0,0 +1,2 @@
+cp /Users/anhtran/IdeaProjects/android-kasse/app/build/outputs/apk/debug/app-debug.apk /Users/anhtran/Dropbox/Kasse/apk/mainapp
+node /Users/anhtran/IdeaProjects/lieferservice/upload 1.02 /Users/anhtran/Dropbox/Kasse/apk/mainapp/app-debug.apk starkasse-main.apk
